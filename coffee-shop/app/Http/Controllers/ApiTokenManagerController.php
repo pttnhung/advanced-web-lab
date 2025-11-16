2de7494b3236c9e5b99db6c25dd24e827d9c46d3
pttnhung <nhung.phan230206@vnuk.edu.vn>
2025-11-17 04:41:49 +0700

    Done

---
+++ coffee-shop/app/Http/Controllers/ApiTokenManagerController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ApiTokenManagerController extends Controller
+{
+    public function index()
+    {
+        return view('api.index');
+    }
+}
+
--- coffee-shop/routes/api.php
+++ coffee-shop/routes/api.php
@@ -3,11 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::prefix('v1')->group(function () {
+Route::prefix('v1')->middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
     Route::apiResource('categories', \App\Http\Controllers\API\CategoryController::class);
     Route::apiResource('products', \App\Http\Controllers\API\ProductController::class);
 });
+
